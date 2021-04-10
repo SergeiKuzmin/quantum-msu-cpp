3e2dff64c2a68b2f2a1436975f218946557ebfdf
Sergei Kuzmin <ssk251198@yandex.ru>
2021-04-11 01:53:56 +0300

    Added task3_2

--- task3/task3_2/second.cpp
+++ task3/task3_2/second.cpp
@@ -2,24 +2,30 @@
 
 class A{
 public:
-virtual ~A(){
-    std::cout << "Destructor of A" << std::endl;
-}
+int a;
 };
 
 class B{
 public:
-~B(){
-    std::cout << "Destructor of B" << std::endl;
-}
+int b;
 };
 
 class C: public A, public B{
+public:
+int c;
 };
 
 int main()
 {
-    C *c = new C;
-    delete c;
+    C c;
+    std::cout << &c << std::endl;
+    std::cout << std::endl;
+    std::cout << static_cast<A*>(&c) << std::endl;
+    std::cout << static_cast<B*>(&c)<< std::endl;
+    std::cout << reinterpret_cast<A*>(&c) << std::endl;
+    std::cout << reinterpret_cast<B*>(&c) << std::endl;
+    std::cout << std::endl;
+    std::cout << (A*)(&c) << std::endl;
+    std::cout << (B*)(&c) << std::endl;
     return 0;
 }
