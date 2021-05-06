f4f6163d01c4fda43e723afc50b4a72901ab6bab
Sergei Kuzmin <ssk251198@yandex.ru>
2021-05-06 13:01:34 +0300

    Written add(), but not working yet

--- task4/state.cpp
+++ task4/state.cpp
@@ -1,11 +1,42 @@
 #include "state.h"
 
+State add(const State &s1, const State &s2)
+{
+    State s3;
+    auto it2_global = s2.begin();
+    for(auto it1 = s1.begin(); it1 != s1.end(); ++it1)
+    {
+        if (it1->first > it2_global->first)
+        {
+            for(auto it2 = it2_global; ((it2->first <= it1->first) && (it2 != s2.end())); it2++)
+            {
+                if (it1->first == it2->first)
+                {
+                    s3.insert(std::pair<Fock, Amp>(it1->first, it1->second + it2->second));
+                } else {
+                    s3.insert(std::pair<Fock, Amp>(it2->first, it2->second));
+                }
+                it2_global = it2;
+            }
+        } else {
+            if (it1->first < it2_global->first)
+            {
+                s3.insert(std::pair<Fock, Amp>(it1->first, it1->second));
+                it2_global++;
+            } else {
+            s3.insert(std::pair<Fock, Amp>(it1->first, it1->second + it2_global->second));
+            it2_global++;
+            }
+        }
+    }
+    return s3;
+}
 
 void print(const State &s)
 {
     for(auto it = s.begin(); it != s.end(); ++it)
     {
-        std::cout << " " << it->first << " = " << it->second << std::endl;
+        std::cout << it->first << " = " << it->second << std::endl;
     }
 }
 
