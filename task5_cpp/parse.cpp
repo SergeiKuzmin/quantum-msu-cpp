70c0a5005c1e08784270c6ed82640a4479e9e03d
Sergei Kuzmin <ssk251198@yandex.ru>
2021-05-23 20:21:50 +0300

    Minor improvements

--- task5_cpp/parse.cpp
+++ task5_cpp/parse.cpp
@@ -20,13 +20,11 @@ int main()
     str_in.read(reinterpret_cast<char*>(&s_header), sizeof(s_header));
     str_in.seekg(s_header.sh_addr + sizeof(Elf64_Sym) * NUM_SYM);
     str_in.read(reinterpret_cast<char*>(&sym), sizeof(sym));
-    if (!str_in.is_open()) std::cout << "Failed" << std::endl;
     str_in.close();
 
     std::cout << "st_other = " << sym.st_other << std::endl;
     sym.st_other = 0;
     std::ofstream str_out(filename_out, std::ios::binary | std::ios::in | std::ios::out);
-    if (!str_out.is_open()) std::cout << "Failed" << std::endl;
     str_out.seekp(s_header.sh_addr + sizeof(Elf64_Sym) * NUM_SYM);
     str_out.write(reinterpret_cast<char*>(&sym), sizeof(sym));
     str_out.close();
