3550b35e6904b5a0d22010a9a49ce103913352c7
Sergei Kuzmin <ssk251198@yandex.ru>
2021-05-24 17:25:31 +0300

    Minor changes

--- task5_cpp/parse.cpp
+++ task5_cpp/parse.cpp
@@ -3,11 +3,10 @@
 #include <string>
 #include "elf.h"
 
-#define NUM_SEC 4
-#define NUM_SYM 6
-
 int main()
 {
+    int num_of_sec = 4;
+    int num_of_sym = 6;
     std::string filename_in = "libsecret.so";
     std::string filename_out = "libsecretnew.so";
     std::ifstream str_in(filename_in, std::ios::binary);
@@ -16,16 +15,15 @@ int main()
     Elf64_Sym sym;
 
     str_in.read(reinterpret_cast<char*>(&e_header), sizeof(e_header));
-    str_in.seekg(e_header.e_shoff + sizeof(Elf64_Shdr) * NUM_SEC);
+    str_in.seekg(e_header.e_shoff + sizeof(Elf64_Shdr) * num_of_sec);
     str_in.read(reinterpret_cast<char*>(&s_header), sizeof(s_header));
-    str_in.seekg(s_header.sh_addr + sizeof(Elf64_Sym) * NUM_SYM);
+    str_in.seekg(s_header.sh_addr + sizeof(Elf64_Sym) * num_of_sym);
     str_in.read(reinterpret_cast<char*>(&sym), sizeof(sym));
     str_in.close();
 
-    std::cout << "st_other = " << sym.st_other << std::endl;
     sym.st_other = 0;
     std::ofstream str_out(filename_out, std::ios::binary | std::ios::in | std::ios::out);
-    str_out.seekp(s_header.sh_addr + sizeof(Elf64_Sym) * NUM_SYM);
+    str_out.seekp(s_header.sh_addr + sizeof(Elf64_Sym) * num_of_sym);
     str_out.write(reinterpret_cast<char*>(&sym), sizeof(sym));
     str_out.close();
     return 0;
